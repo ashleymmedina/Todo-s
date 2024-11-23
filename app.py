8d01482344eaf6cc9b17ef9f6f435d451998393e
ashleymmedina <ashley.meza-medina@csedge.org>
2024-11-23 14:48:38 -0800

    part 3

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request
+from tinydb import TinyDB
 
+db = TinyDB('db.json')
 app = Flask(__name__)
 
 @app.route('/todo', methods=['GET','POST'])
 def hello():
-    todo = request.form.get('todo')
-    print(todo)
-    return render_template('index.html')
+    if request.method == "POST":
+        todo = request.form.get('todo')
+        print("fljsdfjdfjlfdjlnfdslkjsdfnlsflnfksdsfnklfdsklndfsnkl")
+        db.insert({"todo": todo})
+    allTodos = db.all()
+    return render_template('index.html', todos = allTodos)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='8080')
